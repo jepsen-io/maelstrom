ec5bcb9bd8c028dda43a06f6c1179be4c3742539
Aphyr <aphyr@aphyr.com>
2017-04-19 16:32:35 -0500

    rewrite to be singlethreaded

--- demo.rb
+++ demo.rb
@@ -1,16 +1,11 @@
 #!/usr/bin/ruby
 
 require 'json'
-require 'thread'
 require 'set'
 
 class Logger
-  @@lock = Mutex.new
-
   def <<(*args)
-    @@lock.synchronize do
-      STDERR.puts *args
-    end
+    STDERR.puts *args
   end
 end
 
@@ -69,41 +64,36 @@ class Client
   def initialize(logger)
     @node_id = nil
     @logger = logger
-    @lock = Monitor.new
     @next_msg_id = 0
     @handlers = {}
     @callbacks = {}
+
+    @in_buffer = ""
   end
 
   # Generate a fresh message id
   def new_msg_id
-    @lock.synchronize do
-      @next_msg_id += 1
-    end
+    @next_msg_id += 1
   end
 
   # Register a new message type handler
   def on(type, &handler)
-    @lock.synchronize do
-      if @handlers[type]
-        raise "Already have a handler for #{type}!"
-      end
-
-      @handlers[type] = handler
+    if @handlers[type]
+      raise "Already have a handler for #{type}!"
     end
+
+    @handlers[type] = handler
   end
 
   # Send a body to the given node id
   def send!(dest, body)
-    @lock.synchronize do
-      @logger << "Sent #{{dest: dest, src: @node_id, body: body}.inspect}"
-      JSON.dump({dest: dest,
-                 src:  @node_id,
-                 body: body},
-                STDOUT)
-      STDOUT << "\n"
-      STDOUT.flush
-    end
+    @logger << "Sent #{{dest: dest, src: @node_id, body: body}.inspect}"
+    JSON.dump({dest: dest,
+               src:  @node_id,
+               body: body},
+               STDOUT)
+    STDOUT << "\n"
+    STDOUT.flush
   end
 
   # Reply to a request with a response body
@@ -114,36 +104,44 @@ class Client
 
   # Send an RPC request
   def rpc!(dest, body, &handler)
-    @lock.synchronize do
-      msg_id = new_msg_id
-      @callbacks[msg_id] = handler
-      body[:msg_id] = msg_id
-      send! dest, body
+    msg_id = new_msg_id
+    @callbacks[msg_id] = handler
+    body[:msg_id] = msg_id
+    send! dest, body
+  end
+
+  # Consumes available input from stdin, filling @in_buffer. Returns a line
+  # if ready, or nil if no line ready yet.
+  def readline_nonblock
+    begin
+      loop do
+        x = STDIN.read_nonblock 1
+        break if x == "\n"
+        @in_buffer << x
+      end
+
+      res = @in_buffer
+      @in_buffer = ""
+      res
+    rescue IO::WaitReadable
     end
   end
 
   # Starts a thread to handle incoming messages
-  def start!
-    Thread.new do
-      while true
-        begin
-          msg = JSON.parse(STDIN.gets, symbolize_names: true)
-          @logger << "Received #{msg.inspect}"
+  def process_msg!
+    if line = readline_nonblock
+      msg = JSON.parse line, symbolize_names: true
+      @logger << "Received #{msg.inspect}"
 
-          handler = nil
-          @lock.synchronize do
-            if handler = @callbacks[msg[:body][:in_reply_to]]
-              @callbacks.delete msg[:body][:in_reply_to]
-            elsif handler = @handlers[msg[:body][:type]]
-            else
-              raise "No callback or handler for #{msg.inspect}"
-            end
-          end
-          handler.call msg
-        rescue Exception => e
-          @logger << "Error in client input thread! #{e}\n#{e.backtrace.join "\n"}"
-        end
+      handler = nil
+      if handler = @callbacks[msg[:body][:in_reply_to]]
+        @callbacks.delete msg[:body][:in_reply_to]
+      elsif handler = @handlers[msg[:body][:type]]
+      else
+        raise "No callback or handler for #{msg.inspect}"
       end
+      handler.call msg
+      true
     end
   end
 end
@@ -207,7 +205,6 @@ class RaftNode
     @commit_index = 0
     @last_applied = 1
 
-    @lock = Monitor.new
     @client = Client.new @logger
     @state_machine = KVStore.new @logger
     setup_handlers!
@@ -266,67 +263,57 @@ class RaftNode
   end
 
   def advance_term!(term)
-    @lock.synchronize do
-      raise "Can't go backwards" unless @current_term < term
-      @current_term = term
-      @voted_for = nil
-    end
+    raise "Can't go backwards" unless @current_term < term
+    @current_term = term
+    @voted_for = nil
   end
 
 
   ## Transitions between roles ###############################################
 
   def become_follower!
-    @lock.synchronize do
-      @logger << "Became follower for term #{@current_term}"
-      @state = :follower
-      @next_index = nil
-      @match_index = nil
-    end
+    @logger << "Became follower for term #{@current_term}"
+    @state = :follower
+    @next_index = nil
+    @match_index = nil
   end
 
   def become_candidate!
-    @lock.synchronize do
-      @state = :candidate
-      advance_term! @current_term + 1
-      @voted_for = @node_id
-      @logger << "Became candidate for term #{@current_term}"
-      reset_election_deadline!
-      request_votes!
-    end
+    @state = :candidate
+    advance_term! @current_term + 1
+    @voted_for = @node_id
+    @logger << "Became candidate for term #{@current_term}"
+    reset_election_deadline!
+    request_votes!
   end
 
   def become_leader!
-    @lock.synchronize do
-      raise "Should be a candidate" unless @state == :candidate
-      @logger << "Became leader for term #{@current_term}"
-      @state = :leader
-      @next_index = Hash[other_nodes.zip([@log.size + 1] * other_nodes.size)]
-      @match_index = Hash[other_nodes.zip([0] * other_nodes.size)]
-    end
+    raise "Should be a candidate" unless @state == :candidate
+    @logger << "Became leader for term #{@current_term}"
+    @state = :leader
+    @next_index = Hash[other_nodes.zip([@log.size + 1] * other_nodes.size)]
+    @match_index = Hash[other_nodes.zip([0] * other_nodes.size)]
   end
 
   ## Rules for all servers ##################################################
 
   def advance_state_machine!
-    @lock.synchronize do
-      if @last_applied < @commit_index
-        @last_applied += 1
-        res = @state_machine.apply! @log[@last_applied][:op]
-        if @state == :leader
-          @logger << "KV response: #{res.inspect}"
-          @client.send! res[:dest], res[:body]
-        end
+    @logger << "advance_state_machine!"
+    if @last_applied < @commit_index
+      @last_applied += 1
+      res = @state_machine.apply! @log[@last_applied][:op]
+      if @state == :leader
+        @logger << "KV response: #{res.inspect}"
+        @client.send! res[:dest], res[:body]
       end
+      true
     end
   end
 
   def maybe_step_down!(remote_term)
-    @lock.synchronize do
-      if @current_term < remote_term
-        advance_term! remote_term
-        become_follower!
-      end
+    if @current_term < remote_term
+      advance_term! remote_term
+      become_follower!
     end
   end
 
@@ -334,54 +321,46 @@ class RaftNode
   ## Rules for leaders ######################################################
 
   def replicate_log!(force)
-    sent = false
-    @lock.synchronize do
-      if @state == :leader
-        other_nodes.each do |node|
-          ni = @next_index[node]
-          if force or ni <= @log.size
-            entries = @log.from ni
-            @client.rpc!(
-              node,
-              type:            "append_entries",
-              term:            @current_term,
-              leader_id:       @node_id,
-              prev_log_index:  ni - 1,
-              prev_log_term:   @log[ni - 1][:term],
-              entries:         entries,
-              leader_commit:   @commit_index
-            ) do |res|
-              body = res[:body]
-              @lock.synchronize do
-                if body[:success]
-                  @next_index[node] =
-                    [@next_index[node], ni + entries.size].max
-                  @match_index[node] =
-                    [@match_index[node], ni - 1 + entries.size].max
-                else
-                  @next_index[node] -= 1
-                end
-              end
-            end
-            reset_heartbeat_deadline!
-            sent = true
+    @logger << "replicate_log!"
+    if @state == :leader and @heartbeat_deadline <= Time.now
+      @logger << "time to replicate"
+      other_nodes.each do |node|
+        ni = @next_index[node]
+        entries = @log.from ni
+        @logger << "replicating #{ni}+: #{entries.inspect}"
+        @client.rpc!(
+          node,
+          type:            "append_entries",
+          term:            @current_term,
+          leader_id:       @node_id,
+          prev_log_index:  ni - 1,
+          prev_log_term:   @log[ni - 1][:term],
+          entries:         entries,
+          leader_commit:   @commit_index
+        ) do |res|
+          body = res[:body]
+          if body[:success]
+            @next_index[node] =
+              [@next_index[node], ni + entries.size].max
+            @match_index[node] =
+              [@match_index[node], ni - 1 + entries.size].max
+          else
+            @next_index[node] -= 1
           end
         end
       end
-    end
-
-    if sent
-      sleep 1
+      reset_heartbeat_deadline!
+      true
     end
   end
 
   def leader_advance_commit_index!
-    @lock.synchronize do
-      if @state == :leader
-        n = median match_index.values
-        if @commit_index < n and @log[n][:term] == @current_term
-          @commit_index = n
-        end
+    @logger << "leader_advance_commit_index!"
+    if @state == :leader
+      n = median match_index.values
+      if @commit_index < n and @log[n][:term] == @current_term
+        @commit_index = n
+        true
       end
     end
   end
@@ -392,89 +371,43 @@ class RaftNode
   def request_votes!
     votes = Set.new([@node_id])
 
-    @lock.synchronize do
-      brpc!(
-        type:            "request_vote",
-        term:            @current_term,
-        candidate_id:    @node_id,
-        last_log_index:  @log.size,
-        last_log_term:   @log.last_term
-      ) do |response|
-        body = response[:body]
-        @lock.synchronize do
-          case body[:type]
-          when "request_vote_res"
-            maybe_step_down! body[:term]
-            if @state == :candidate and body[:vote_granted] and body[:term] == @current_term
-              # Got a vote for our candidacy
-              votes << response[:src]
-              if majority(@node_ids.size) <= votes.size
-                # We have a majority of votes for this term
-                become_leader!
-              end
-            end
-          else
-            raise "Unknown response message: #{response.inspect}"
+    brpc!(
+      type:            "request_vote",
+      term:            @current_term,
+      candidate_id:    @node_id,
+      last_log_index:  @log.size,
+      last_log_term:   @log.last_term
+    ) do |response|
+      body = response[:body]
+      case body[:type]
+      when "request_vote_res"
+        maybe_step_down! body[:term]
+        if @state == :candidate and body[:vote_granted] and body[:term] == @current_term
+          # Got a vote for our candidacy
+          votes << response[:src]
+          if majority(@node_ids.size) <= votes.size
+            # We have a majority of votes for this term
+            become_leader!
           end
         end
+      else
+        raise "Unknown response message: #{response.inspect}"
       end
     end
   end
 
-
-  def heartbeat!
-    @lock.synchronize do
-      if @state == :leader
-        dt = @heartbeat_deadline - Time.now
-        if (0 < dt)
-          sleep dt
-        else
-          replicate_log! true
-        end
+  def election!
+    @logger << "election!"
+    dt = (@election_deadline - Time.now)
+    if (dt <= 0)
+      if (@state == :follower or @state == :candidate)
+        # Time for an election!
+        become_candidate!
+      else
+        # We're a leader or initializing, sleep again
+        reset_election_deadline!
       end
-    end
-  end
-
-  ## Threads ###############################################################
-
-  # Spawns a thread to periodically perform elections
-  def election_thread!
-    Thread.new do
-      while true
-        begin
-          dt = (@election_deadline - Time.now)
-          if (dt <= 0)
-            if (@state == :follower or @state == :candidate)
-              # Time for an election!
-              become_candidate!
-            else
-              # We're a leader or initializing, sleep again
-              reset_election_deadline!
-            end
-          else
-            sleep dt
-          end
-        rescue Exception => e
-          @logger << "Election thread caught #{e}:\n#{e.backtrace.join("\n")}"
-        end
-      end
-    end
-  end
-
-  # Spawns a thread to replicate the leader's log
-  def transition_thread!
-    Thread.new do
-      while true
-        begin
-          replicate_log! false
-          heartbeat!
-          leader_advance_commit_index!
-          advance_state_machine!
-          sleep 0.2
-        rescue Exception => e
-          @logger << "Caught #{e}:\n#{e.backtrace.join "\n"}"
-        end
-      end
+      true
     end
   end
 
@@ -482,81 +415,72 @@ class RaftNode
 
   def add_log_entry!(msg)
     body = msg[:body]
-    @lock.synchronize do
-      if @state == :leader
-        body[:client] = msg[:src]
-        @log << {term: @current_term, op: body}
-      else
-        @client.reply! msg, {type: "error", code: 11, text: "not a leader"}
-      end
+    if @state == :leader
+      body[:client] = msg[:src]
+      @log << {term: @current_term, op: body}
+    else
+      @client.reply! msg, {type: "error", code: 11, text: "not a leader"}
     end
   end
 
   def setup_handlers!
     @client.on "raft_init" do |msg|
-      @lock.synchronize do
-        raise "Can't init twice!" unless @state == :nascent
+      raise "Can't init twice!" unless @state == :nascent
 
-        body = msg[:body]
-        self.node_id = body[:node_id]
-        @node_ids = body[:node_ids]
-        @logger << "Raft init!"
-        @client.reply! msg, {type: "raft_init_ok"}
+      body = msg[:body]
+      self.node_id = body[:node_id]
+      @node_ids = body[:node_ids]
+      @logger << "Raft init!"
+      @client.reply! msg, {type: "raft_init_ok"}
 
-        reset_election_deadline!
-        become_follower!
-        election_thread!
-      end
+      reset_election_deadline!
+      become_follower!
     end
 
     @client.on "request_vote" do |msg|
       body = msg[:body]
-      @lock.synchronize do
-        maybe_step_down! body[:term]
-        grant = false
-        if (body[:term] < @current_term)
-        elsif (@voted_for == nil or @voted_for == body[:candidate_id]) and
-          @log.last_term <= body[:last_log_term] and
-          @log.size <= body[:last_log_index]
+      maybe_step_down! body[:term]
+      grant = false
+      if (body[:term] < @current_term)
+      elsif (@voted_for == nil or @voted_for == body[:candidate_id]) and
+        @log.last_term <= body[:last_log_term] and
+        @log.size <= body[:last_log_index]
 
-          grant = true
-          @voted_for = body[:candidate_id]
-          reset_election_deadline!
-        end
-
-        @client.reply! msg, {type: "request_vote_res",
-                             term: @current_term,
-                             vote_granted: grant}
+        grant = true
+        @voted_for = body[:candidate_id]
+        reset_election_deadline!
       end
+
+      @client.reply! msg, {type: "request_vote_res",
+                           term: @current_term,
+                           vote_granted: grant}
     end
 
     @client.on "append_entries" do |msg|
       body = msg[:body]
-      @lock.synchronize do
-        maybe_step_down! body[:term]
-        reset_election_deadline!
+      maybe_step_down! body[:term]
+      reset_election_deadline!
 
-        ok  = {type: "append_entries_res", term: @current_term, success: true}
-        err = {type: "append_entries_res", term: @current_term, success: false}
-        if body[:term] < @current_term
-          # Leader is behind us
-          @client.reply! msg, err
-        elsif 0 < body[:prev_log_index] and e = @log[body[:prev_log_index]] and (e.nil? or e[:term] != body[:prev_log_term])
-          # We disagree on the previous log term
-          @client.reply! msg, err
-        else
-          # OK, we agree on the previous log term now. Truncate and append
-          # entries.
-          @log.truncate body[:prev_log_index]
-          @log.append body[:entries]
+      ok  = {type: "append_entries_res", term: @current_term, success: true}
+      err = {type: "append_entries_res", term: @current_term, success: false}
+      if body[:term] < @current_term
+        # Leader is behind us
+        @client.reply! msg, err
+      elsif 0 < body[:prev_log_index] and e = @log[body[:prev_log_index]] and (e.nil? or e[:term] != body[:prev_log_term])
+        # We disagree on the previous log term
+        @client.reply! msg, err
+      else
+        # OK, we agree on the previous log term now. Truncate and append
+        # entries.
+        @log.truncate body[:prev_log_index]
+        @log.append body[:entries]
 
-          # Advance commit pointer
-          if @commit_index < body[:leader_commit]
-            @commit_index = [body[:leader_commit], @log.size].min
-          end
-
-          @client.reply! msg, ok
+        # Advance commit pointer
+        if @commit_index < body[:leader_commit]
+          @commit_index = [body[:leader_commit], @log.size].min
         end
+
+        @client.reply! msg, ok
       end
     end
 
@@ -565,21 +489,22 @@ class RaftNode
     @client.on "cas"   do |msg| add_log_entry! msg end
   end
 
-
-  ## Lifecycle ##############################################################
-
-  def start!
-    begin
-      @client.start!
-      transition_thread!
-      @logger << "Online"
-      while true
-        sleep 1
+  def main!
+    @logger << "Online"
+    while true
+      begin
+        @logger << "Loop"
+        @client.process_msg! or
+          replicate_log! false or
+          election! or
+          leader_advance_commit_index! or
+          advance_state_machine! or
+          sleep 0.1
+      rescue StandardError => e
+        @logger << "Caught #{e}:\n#{e.backtrace.join "\n"}"
       end
-    rescue Exception => e
-      @logger << "Error starting node: #{e}"
     end
   end
 end
 
-RaftNode.new.start!
+RaftNode.new.main!
