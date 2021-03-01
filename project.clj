05ec6d7f68e9a90f73b053c0114429b4964f8d94
Aphyr <aphyr@aphyr.com>
2021-03-01 17:38:03 -0500

    Jepsen 0.2.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main maelstrom.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.3-SNAPSHOT"]
+                 [jepsen "0.2.3"]
                  [amalloy/ring-buffer "1.3.1"]
                  [cheshire "5.7.0"]
                  [byte-streams "0.2.2"]
