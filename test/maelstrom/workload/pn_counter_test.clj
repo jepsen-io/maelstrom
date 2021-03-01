54e7810bd881c21258a2052af60710d131eef9ba
Aphyr <aphyr@aphyr.com>
2021-03-01 17:16:11 -0500

    pn-counter-test: update to include final-read keys

--- test/maelstrom/workload/pn_counter_test.clj
+++ test/maelstrom/workload/pn_counter_test.clj
@@ -9,12 +9,14 @@
     (testing "empty"
       (is (= {:valid?     true
               :errors     nil
+              :final-reads []
               :acceptable [[0 0]]}
              (check []))))
 
     (testing "definite"
       (is (= {:valid?     false
               :errors     [{:type :ok, :f :read, :final? true, :value 4}]
+              :final-reads [5 4]
               :acceptable [[5 5]]}
              (check [{:type :ok, :f :add, :value 2}
                      {:type :ok, :f :add, :value 3}
@@ -24,6 +26,7 @@
     (testing "indefinite"
       (is (= {:valid?     false
               :errors     [{:type :ok, :f :read, :final? true, :value 11}]
+              :final-reads [11 15]
               :acceptable [[8 10] [13 15]]}
              (check [{:type :ok,   :f :add, :value 10}
                      {:type :info, :f :add, :value 5}
