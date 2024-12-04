ec124ddf69c07c1c7e82451f5b373b73ae751473
Kyle Kingsbury <aphyr@aphyr.com>
2024-12-04 12:24:29 -0600

    Version 0.2.4, with dep upgrades

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject maelstrom "0.2.4-SNAPSHOT"
+(defproject maelstrom "0.2.4"
   :description "A test bench for writing toy distributed systems"
   :url "https://github.com/jepsen-io/maelstrom"
   :license {:name "Eclipse Public License"
@@ -7,10 +7,10 @@
   :jvm-opts ["-Djava.awt.headless=true"
              ; "-agentpath:/home/aphyr/yourkit/bin/linux-x86-64/libyjpagent.so=sampling,exceptions=disable,probe_disable=*"
              ]
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [jepsen "0.3.7"]
                  [amalloy/ring-buffer "1.3.1"]
-                 [cheshire "5.11.0"]
+                 [cheshire "5.13.0"]
                  [byte-streams "0.2.4"]
                  ; Reductions over journals
                  [tesser.core "1.0.6"]
